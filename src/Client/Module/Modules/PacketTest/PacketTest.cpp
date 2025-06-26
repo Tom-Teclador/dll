fb3feca0662ad4a39f4541c439808a41b39bef04
Tom Teclador <tom@dst-lenz.de>
2025-06-26 15:46:13 +0200

    beta: packet UpdateAttributes test

--- src/Client/Module/Modules/PacketTest/PacketTest.cpp
+++ src/Client/Module/Modules/PacketTest/PacketTest.cpp
@@ -21,6 +21,24 @@ void PacketTest::defaultConfig() {
 
 void PacketTest::settingsRender(float settingsOffset) {
 
+
+    float x = Constraints::PercentageConstraint(0.019, "left");
+    float y = Constraints::PercentageConstraint(0.10, "top");
+
+    const float scrollviewWidth = Constraints::RelativeConstraint(0.12, "height", true);
+
+
+    FlarialGUI::ScrollBar(x, y, 140, Constraints::SpacingConstraint(5.5, scrollviewWidth), 2);
+    FlarialGUI::SetScrollView(x - settingsOffset, Constraints::PercentageConstraint(0.00, "top"),
+        Constraints::RelativeConstraint(1.0, "width"),
+        Constraints::RelativeConstraint(0.88f, "height"));
+
+    addHeader("Auto GG");
+    addTextBox("Text", "The text to send.", 0, "text");
+
+    FlarialGUI::UnsetScrollView();
+
+    resetPadding();
 }
 
 void PacketTest::onPacketReceive(PacketEvent& event) {
